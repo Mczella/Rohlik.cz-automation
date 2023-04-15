c9a79868a06fb6206b8239947f47a43bc008d483
Mczella <markaczova@gmail.com>
2023-04-15 20:58:04 +0200

    Delete shopping_list.py and try.py

--- shopping_list.py
+++
@@ -1,5 +0,0 @@
-def generate_shopping_list(weekly_meal_plan, food_inventory):
-    shopping_list = {}
-    # shipping_list[weekly_meal_plan[0]["ingredients"][0]] = weekly_meal_plan[0]["ingredients"][1]
-    for key, value in weekly_meal_plan[0]["ingredients"].items():
-        shipping_list[key] = value
